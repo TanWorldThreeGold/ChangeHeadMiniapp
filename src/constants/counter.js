9dec93b92a9dab43d9de394bf62a1ae2265097cc
han <18503030864@163.com>
2020-01-18 17:17:55 +0800

    react game

--- src/constants/counter.js
+++
@@ -1,7 +0,0 @@
-// import * as YuanConstants from './yuan_constants.js'
-
-export const ADD = 'ADD'
-export const MINUS = 'MINUS'
-
-// yuan
-export * from './yuan_constants.js'
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import counter from './counter'
-import headList from './yuan_head_image_list'
-
-export default combineReducers({
-  counter,
-  headList
-})
